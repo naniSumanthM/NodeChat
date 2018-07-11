804b76acb773d5eac1ba1d27838986f82a2d9d74
smaddirala <smaddirala@gaig.com>
2018-07-11 10:54:15 -0400

    emmiting events from server

--- server/server.js
+++ server/server.js
@@ -12,12 +12,36 @@ let io = socketIO(server)
 app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
-    console.log('New User Connected');
+    console.log('New client connected!');
+
+    //new Email emitted by the server
+    socket.emit('newEmail', {
+        from: "maddirsh@yahoo.com",
+        text: "Test from server",
+        createdAt: "today"
+    })
+
+    //Server listening to createEmail event triggered from the client
+    socket.on('createEmail', (newEmail) => {
+        console.log(newEmail);
+    })
+
+    //Server emitting the newMessage
+    socket.emit('newMessage', {
+        from: "admin",
+        text: "Message from server admin",
+        createdAt: "Today"
+    })
+
+    //Server listening to createMessage event triggered from the client
+    socket.on('createMessage', (message) => {
+        console.log(message);
+    })
+
     socket.on('disconnect', () => {
-        console.log('Disconnected from the client');
+        console.log('Sever disconnected from the client!');
     })
 })
-
 
 server.listen(port, () => {
     console.log(`Server live on ${port}`);
