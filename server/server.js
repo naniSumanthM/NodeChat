f266af1593c63a0eaccfa6957c7073ec327c7111
smaddirala <smaddirala@gaig.com>
2018-07-11 11:52:12 -0400

    refactored function for emitting msg

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const { generateMessage } = require('./utils/message')
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public')
 let app = express()
@@ -16,26 +17,14 @@ io.on('connection', (socket) => {
     console.log('New client connected!');
 
     //alerts all new clients of joining
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', "Welcome to the chat app!"));
 
     //alert every other user of new user joining other than the new user
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', "New user joined!"));
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text))
     });
 
     socket.on('disconnect', () => {
