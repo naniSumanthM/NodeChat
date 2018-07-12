70fd01d7dcd9536748f53144a79e28533d67ebfd
smaddirala <smaddirala@gaig.com>
2018-07-12 15:16:47 -0400

    messages formatted

--- public/js/index.js
+++ public/js/index.js
@@ -12,20 +12,28 @@ socket.on('disconnect', function () {
 //client renders new message to window
 socket.on('newMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm:a')
-    let li = jQuery('<li></li>');
-    li.text(`${message.from}: ${formattedTime}-->${message.text}`)
-    jQuery('#messages').append(li)
+    let template = jQuery('#message-template').html();
+
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    })
+    jQuery('#messages').append(html)
 })
 
 //renders the current loaction with a link
 socket.on('newLocationMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm:a')
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">Current Location</a>')
-    li.text(`${message.from}: ${formattedTime} `)
-    a.attr('href', message.url)
-    li.append(a)
-    jQuery('#messages').append(li)
+    let template = jQuery("#location-message-template").html()
+
+    let html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    })
+
+    jQuery('#messages').append(html)
 })
 
 //client creates new message
